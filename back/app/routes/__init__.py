3cd15b2c8f816841036a78cfae3a3653ec1c83b5
rmnkubyshkin <rm.kubyshkin@ya.ru>
2026-04-18 09:59:26 +0300

    feat: Добавлен ендпоинт для получения детальной инфомрации по заведению

--- back/app/routes/__init__.py
+++ back/app/routes/__init__.py
@@ -89,16 +89,75 @@ def get_geo_coordinates():
 
 @bp.route("/api/points/h3/indexes/simple", methods=["GET"])
 def get_h3_indexes_simple():
+    
     if not ch.client:
         return jsonify({"error": "Database connection error"}), 500
     
     limit = request.args.get('limit', 10000, type=int)
     
     try:
-        result = ch.client.query(f"SELECT h3_index, altitude FROM points_h3 LIMIT {limit}")
+        result = ch.client.query(f"SELECT h3_index, altitude FROM places_h3 LIMIT {limit}")
         
         data = [{"h3Index": str(row[0]), "altitude": row[1]} for row in result.result_rows]
         return jsonify(data)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+@bp.route("/api/places/<place_id>", methods=["GET"])
+def get_place_details(place_id):
+    if not ch.client:
+        return jsonify({"error": "Database connection error"}), 500
+
+    try:
+        result = ch.client.query("""
+            SELECT 
+                place_id,
+                any(name) as name,
+                any(primary_type) as primary_type,
+                any(types) as types,
+
+                any(latitude) as latitude,
+                any(longitude) as longitude,
+                any(altitude) as altitude,
+
+                any(rating) as rating,
+                any(user_ratings_total) as user_ratings_total,
+                any(price_level) as price_level,
+
+                any(open_now) as open_now,
+                any(business_status) as business_status,
+
+                any(created_at) as created_at
+            FROM points_h3
+            WHERE place_id = %(place_id)s
+            GROUP BY place_id
+            LIMIT 1
+        """, {"place_id": place_id})
+
+        if not result.result_rows:
+            return jsonify({"error": "Place not found"}), 404
+
+        row = result.result_rows[0]
+
+        return jsonify({
+            "place_id": row[0],
+            "name": row[1],
+            "primary_type": row[2],
+            "types": row[3],
+
+            "latitude": row[4],
+            "longitude": row[5],
+            "altitude": row[6],
+
+            "rating": row[7],
+            "user_ratings_total": row[8],
+            "price_level": row[9],
+
+            "open_now": bool(row[10]),
+            "business_status": row[11],
+
+            "created_at": row[12]
+        })
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
